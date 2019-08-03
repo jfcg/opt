579811b29df103f92d0957d8fd45633a1b682c24
Serhat Şevki Dinçer <jfcgauss@gmail.com>
2019-08-03 23:54:46 +0300

    simplify

--- min.go
+++ min.go
@@ -1,6 +1,39 @@
 // Package opt provides some optimization routines
 package opt
 
+func shift3x3grid(k int, fv *[9]float64) {
+	switch k {
+	case 0: // down left
+		fv[4], fv[5], fv[7], fv[8] = fv[0], fv[1], fv[3], fv[4]
+		fv[0], fv[1], fv[2], fv[3], fv[6] = 0, 0, 0, 0, 0
+	case 2: // down right
+		fv[3], fv[4], fv[6], fv[7] = fv[1], fv[2], fv[4], fv[5]
+		fv[0], fv[1], fv[2], fv[5], fv[8] = 0, 0, 0, 0, 0
+	case 6: // up left
+		fv[1], fv[2], fv[4], fv[5] = fv[3], fv[4], fv[6], fv[7]
+		fv[0], fv[3], fv[6], fv[7], fv[8] = 0, 0, 0, 0, 0
+	case 8: // up right
+		fv[0], fv[1], fv[3], fv[4] = fv[4], fv[5], fv[7], fv[8]
+		fv[2], fv[5], fv[6], fv[7], fv[8] = 0, 0, 0, 0, 0
+	case 1: // down
+		fv[6], fv[7], fv[8] = fv[3], fv[4], fv[5]
+		fv[3], fv[4], fv[5] = fv[0], fv[1], fv[2]
+		fv[0], fv[1], fv[2] = 0, 0, 0
+	case 3: // left
+		fv[2], fv[5], fv[8] = fv[1], fv[4], fv[7]
+		fv[1], fv[4], fv[7] = fv[0], fv[3], fv[6]
+		fv[0], fv[3], fv[6] = 0, 0, 0
+	case 5: // right
+		fv[0], fv[3], fv[6] = fv[1], fv[4], fv[7]
+		fv[1], fv[4], fv[7] = fv[2], fv[5], fv[8]
+		fv[2], fv[5], fv[8] = 0, 0, 0
+	default: // up
+		fv[0], fv[1], fv[2] = fv[3], fv[4], fv[5]
+		fv[3], fv[4], fv[5] = fv[6], fv[7], fv[8]
+		fv[6], fv[7], fv[8] = 0, 0, 0
+	}
+}
+
 // FindMin minimizes positive fn() over (x,y) grid starting at (x0,y0) with (±dx,±dy)
 // steps for up to r runs, halving step sizes between runs. If given pr(), calls it at
 // every new optimal value (could be used for printing progress). Calls fn() at least once.
@@ -73,37 +106,7 @@ func FindMin(r, x0, y0, dx, dy int, fn func(x, y int) float64,
 				pr(x0, y0, fv[k])
 			}
 
-			// update fv grid
-			switch k {
-			case 0: // down left
-				fv[4], fv[5], fv[7], fv[8] = fv[0], fv[1], fv[3], fv[4]
-				fv[0], fv[1], fv[2], fv[3], fv[6] = 0, 0, 0, 0, 0
-			case 2: // down right
-				fv[3], fv[4], fv[6], fv[7] = fv[1], fv[2], fv[4], fv[5]
-				fv[0], fv[1], fv[2], fv[5], fv[8] = 0, 0, 0, 0, 0
-			case 6: // up left
-				fv[1], fv[2], fv[4], fv[5] = fv[3], fv[4], fv[6], fv[7]
-				fv[0], fv[3], fv[6], fv[7], fv[8] = 0, 0, 0, 0, 0
-			case 8: // up right
-				fv[0], fv[1], fv[3], fv[4] = fv[4], fv[5], fv[7], fv[8]
-				fv[2], fv[5], fv[6], fv[7], fv[8] = 0, 0, 0, 0, 0
-			case 1: // down
-				fv[6], fv[7], fv[8] = fv[3], fv[4], fv[5]
-				fv[3], fv[4], fv[5] = fv[0], fv[1], fv[2]
-				fv[0], fv[1], fv[2] = 0, 0, 0
-			case 3: // left
-				fv[2], fv[5], fv[8] = fv[1], fv[4], fv[7]
-				fv[1], fv[4], fv[7] = fv[0], fv[3], fv[6]
-				fv[0], fv[3], fv[6] = 0, 0, 0
-			case 5: // right
-				fv[0], fv[3], fv[6] = fv[1], fv[4], fv[7]
-				fv[1], fv[4], fv[7] = fv[2], fv[5], fv[8]
-				fv[2], fv[5], fv[8] = 0, 0, 0
-			default: // up
-				fv[0], fv[1], fv[2] = fv[3], fv[4], fv[5]
-				fv[3], fv[4], fv[5] = fv[6], fv[7], fv[8]
-				fv[6], fv[7], fv[8] = 0, 0, 0
-			}
+			shift3x3grid(k, &fv)
 		}
 
 		dx /= 2 // halve steps
